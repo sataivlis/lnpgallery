437976a583e34fe51f2360aed3803880b0c209fe
sataivlis <yjie@ucmerced.edu>
2019-09-06 22:14:45 -0700

    added filter cards by character/css ver 1.0

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
   path('', views.gallery, name='gallery'),
+  path('character/<int:pk>', views.chara_card, name='chara_card'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Character, Card
 
 def gallery(request):
-    card_list = Card.objects.all()
 
-    return render(request, 'gallery/gallery.html', {"card_list" : card_list})
+    all_list = {"card_list": Card.objects.all(),
+                "character_name": Character.objects.all(),
+    }
+    return render(request, 'gallery/gallery.html', all_list)
+
+def chara_card(request, pk):
+    all_list = {"character_filter": get_object_or_404(Character, pk=pk),
+            "card_list": Card.objects.filter(card_character=pk),
+            "character_name": Character.objects.all(),
+            "pk": pk,
+    }
+    return render(request, 'gallery/chara_card.html', all_list)
